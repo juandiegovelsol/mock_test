1826fb38c4a61d2d67839bc6995370b904c20121
Juan <juandiegovelsol@gmail.com>
2024-03-28 16:19:27 -0500

    feat: test conection

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,26 +1,28 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 import {
   getAllTasks,
   getTaskById,
   createTask,
   updateTask,
   deleteTask,
-} from './services/TaskService';
+} from "./services/TaskService";
 
 const router = express.Router();
 
-router.get('/healthcheck', (_: Request, res: Response) => {
-  res.status(202).json({ message: 'Server is Ok' });
+router.get("/healthcheck", (_: Request, res: Response) => {
+  res.status(202).json({ message: "Server is Ok" });
 });
 
-router.get('/', (req: Request, res: Response) => {});
+router.get("/", (req: Request, res: Response) => {
+  console.log("this is a test");
+});
 
-router.get('/:id', (req: Request, res: Response) => {});
+router.get("/:id", (req: Request, res: Response) => {});
 
-router.post('/', (req: Request, res: Response) => {});
+router.post("/", (req: Request, res: Response) => {});
 
-router.put('/:id', (req: Request, res: Response) => {});
+router.put("/:id", (req: Request, res: Response) => {});
 
-router.delete('/:id', (req: Request, res: Response) => {});
+router.delete("/:id", (req: Request, res: Response) => {});
 
 export default router;
